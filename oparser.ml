e9ba810a4d88dbe141ce3e588383ca23a5dfdfcf
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2022-11-23 12:17:21 +0100

    do not run "main" in utop

--- oparser.ml
+++ oparser.ml
@@ -1,4 +1,9 @@
 open Processing
 
-let m () =
-  Processing.doit ()
+let isUtop () =
+  String.split_on_char '/' Sys.executable_name
+  |> List.exists (fun x -> x  = "utop")
+
+let () =
+  if  not(isUtop ()) then
+    Processing.doit ()
