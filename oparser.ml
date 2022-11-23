0a2d38b63bbbaaf0af65ef56c096de32aae71974
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2022-11-23 22:30:59 +0100

    refactor checking for utop

--- oparser.ml
+++ oparser.ml
@@ -1,9 +1,10 @@
 open Processing
 
-let isUtop () =
+let not_utop () =
   String.split_on_char '/' Sys.executable_name
   |> List.exists (fun x -> x  = "utop")
+  |> not
 
 let () =
-  if  not(isUtop ()) then
+  if  not_utop () then
     Processing.doit ()
